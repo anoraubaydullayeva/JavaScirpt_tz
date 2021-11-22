a6b18cb1ae6cfbe6ce484ff0339337b60eddba86
anoraubaydullayeva <anoraubaydullayeva9@gmail.com>
2021-11-22 17:01:33 +0500

    tz16 готово

--- js/example.js
+++ js/example.js
@@ -1,55 +1,86 @@
-let films = [
+movies = [
     {
-        time: "10:00",
-        name:  "Человек-паук",
-        genre: "Фантастика, боевик, приключения",
+        adult:true,
+        time: "16:00",
+        name: "Люди в чёрном: Интэрнэшнл",          /*0*/
+        genre: "Фантастика, боевик, комедия",
     },
-    
-    {
-        time: " 12:00",
-        name:  "Собачья жизнь 2",
-        genre: "Фэнтэзи, драма, комедия",
-    },
-    
+
     {
         time: "14:00",
-        name:  "История игрушек 4",
+        name: "История игрушек 4",                  /*1*/
         genre: "Мультфильм, фэнтэзи, комедия",
     },
-    
+
     {
-        time: "16:00",
-        name:  "Люди в чёрном: Интэрнэшнл",
-        genre: "Фантастика, боевик, комедия",
+        time: "12:00",
+        name: "Собачья жизнь 2",
+        genre: "Фэнтэзи, драма, комедия",
+    },
+
+    {
+        adult: true,
+        time: "10:00",
+        name: "Человек-паук 2",
+        genre: "Фантастика, боевик, приключения",
+    },
+    {
+        time: "10:00",
+        name: "Человек-паук",
+        genre: "Фантастика, боевик, приключения",
     },
 ];
 
-let table_body = document.getElementById('film__table');
+table_body = document.getElementById('film__table')
 
-for(i = 0; i < 4; i++){
+const film_helper =
+{
+    getTitle()
+    {
+        return this.name;
+    },
 
-    table_body.innerHTML += `
-   <tr>  
-    <td class="table__row">
-    <img class="ckeck__mark" src="./image/check__mark.png" alt="check_mark">
+    getTime()
+    {
+        return this.time;
+    },
+
+    getGeneres()
+    {
+        return this.genre;
+    }
+}
+
+function generator_film_row(film)
+{
+    return `<tr class = "table__row">
+    <td class="ckeck__mark">
+        <svg width="15" height="11" viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z" fill="#A3CC40"/>
+        </svg>
+    
     </td>
-    <td>  ${films[i].time}</td>
-   
-    <td> ${films[i].name}</td>
-    
-    <td>${films[i].genre}</td>
-    
-    </tr>
-    `
+
+    <td>
+        ${film_helper.getTime.apply(film)}
+    </td>
+    <td>
+        ${film_helper.getTitle.apply(film)}
+    </td>
+    <td>
+        ${film_helper.getGeneres.apply(film)}
+    </td>
+
+    </tr>`
 }
 
-let person =["person", 12, true ]
-
-for( i =0; i < person.length; i++){
-if(i%2===0){console.log(i, person[i])}
+for (film of movies) 
+{
+    if(!film.adult)
+    {    
+        table_body.innerHTML += generator_film_row(film);
+    }
 }
 
-let b=1;
-if(b%2===0){console.log("juft")}
-else(console.log("tog"))
+
 
--- js/main.js
+++ js/main.js
@@ -8,14 +8,50 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        500:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
+        1024:{
             items:3
         }
     }
 })
+
+const menu =document.querySelector(".menu")
+const burgerMenu =document.querySelector(".burger")
+const menuClose =menu.querySelector(".menu__close")
+
+function handleOpenMenu() {
+    menu.classList.add("active")
+}
+
+burgerMenu.addEventListener("click" , handleOpenMenu)
+function handleCloseMenu() {
+    menu.classList.remove("active")
+}
+menuClose.addEventListener("click" , handleCloseMenu)
+
+// function toggleLanguage() {
+//     const lngSelect =document.querySelector(".block01__lng-select")
+//     const buttons =document.querySelectorAll(".block01__dropdown button")
+//     for(let i =0; i < buttons.length; i++){
+//         buttons[i].addEventListener('click', function () {
+//         lngSelect.innerHTML = buttons[i].innerHTML 
+
+//         })
+//     }
+    
+// }
+function toggleLanguage() {
+    const lngSelect =document.querySelector(".block01__lng-select")
+    const buttons =document.querySelector(".block01__dropdown ") 
+  buttons.addEventListener("click", function (e) {
+      lngSelect.innerHTML = e.target.value
+  } )
+}
+ toggleLanguage()
+
+ let person =["per", 12, true ]
+for( i =0; i < person.length; i++){
+if(i%2===0){console.log(i, person[i])}
+}
+let b=1;
+if(b%2===0){console.log("even")}
+else(console.log("odd"))
